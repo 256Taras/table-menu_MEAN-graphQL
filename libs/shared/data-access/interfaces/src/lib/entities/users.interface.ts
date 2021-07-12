ea7e5f2d49b16a42b778079d62c6b7ef438d5ae2
Taras Ivaniuk <tarasivaniuk37@gmail.com>
2021-07-12 13:20:13 +0300

    feat(data-access-interfaces):add tags [WiP]
    * add entities user
    
    #12

---
+++ libs/shared/data-access/interfaces/src/lib/entities/users.interface.ts
@@ -0,0 +1,44 @@
+/**
+ * interface user
+ * @description
+ *
+ * provides an entity interface user
+ */
+export class IUser {
+  /**
+   * @description
+   * user ID
+   */
+  id: number;
+
+  /**
+   * @description
+   * user ID
+   */
+  email: string;
+
+  /**
+   * @description
+   * user ID
+   */
+  password: string;
+  phone?: string;
+
+  /**
+   * @description
+   * user ID
+   */
+  created: string;
+
+  /**
+   * @description
+   * user ID
+   */
+  updated: string;
+
+  /**
+   * @description
+   * user ID
+   */
+  username: string;
+}
