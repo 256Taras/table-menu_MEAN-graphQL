31ae571060141037dfa92846b7b626076ae92132
Taras Ivaniuk <tarasivaniuk37@gmail.com>
2021-07-14 02:39:28 +0300

    feat(api-auth): add auth module [WiP]
    * add jwt strategy
     * add environment.ts jwt section and
         entities ,migrations,cli path
    
    #12

--- apps/backend/api/src/environments/environment.ts
+++ apps/backend/api/src/environments/environment.ts
@@ -1,11 +1,24 @@
 export const environment = {
-  production: false,
-  connection: (configService)=>({
-    type: 'postgres'as 'aurora-data-api',
-    url: configService.get('DATABASE_URL'),
-    autoLoadEntities: true,
-    synchronize: true,
-    logging: false,
-    dropSchema: false,
-  }),
-};
+    production: false,
+    connection: (configService) => ({
+      type: 'postgres' as 'aurora-data-api',
+      url: configService.get('DATABASE_URL'),
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: false,
+      dropSchema: false,
+    }),
+    jwt: {
+      secrete: process.env.JWT_SECRET,
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    },
+    entities: [
+      'dist/apps/backend/api/src/**/*.entity{.ts, .js}',
+    ],
+    migrations: [
+      'dist/apps/backend/api/migrations/*{.ts, .js}',
+    ],
+    cli: {
+      'migrationsDir': 'db/migrations',
+    },
+  };
