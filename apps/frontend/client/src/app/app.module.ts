274c507d6ae41b5921c536eaff81e3286ed10848
Taras Ivaniuk <tarasivaniuk37@gmail.com>
2021-07-23 23:11:48 +0300

    feat(frontend-client): add SSR dep [WiP]
    * add ssr dependencies
    * add ngrx store dependencies
    
    #18

--- apps/frontend/client/src/app/app.module.ts
+++ apps/frontend/client/src/app/app.module.ts
@@ -2,14 +2,14 @@ import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 
 import {AppComponent} from "./app.component";
-import {UiComponentModule} from "@mean/ui-component";
+
 
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    BrowserModule,
-    UiComponentModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+
 
   ],
   providers: [],
