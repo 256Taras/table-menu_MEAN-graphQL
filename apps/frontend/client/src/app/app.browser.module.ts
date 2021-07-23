cf602745910ed61a0490d2861232ce5b37784f5a
Taras Ivaniuk <tarasivaniuk37@gmail.com>
2021-07-23 23:37:17 +0300

    feat(ws * frontend-client): add SSR dep [WiP]
    * add package.json script
    * add app.browser.module.ts
    * change angular.json main.ts to main.browser.ts
    * add in app.module.ts -> RouterModule
    * add app.server.module.ts
    * change tsconfig.app.json field "files" -> main.ts to main.browser.ts
    
    Closes #18

---
+++ apps/frontend/client/src/app/app.browser.module.ts
@@ -0,0 +1,17 @@
+import {NgModule} from "@angular/core";
+import {AppModule} from "./app.module";
+import {AppComponent} from "./app.component";
+import {environment} from "../environments/environment";
+import {StoreDevtoolsModule} from "@ngrx/store-devtools";
+
+
+@NgModule({
+  imports: [
+    AppModule,
+    !environment.production ? StoreDevtoolsModule.instrument({logOnly: environment.production}) : []
+  ],
+  providers: [],
+  bootstrap:[AppComponent]
+})
+export class AppBrowserModule {
+}
--- apps/frontend/client/src/app/app.module.ts
+++ apps/frontend/client/src/app/app.module.ts
@@ -2,18 +2,17 @@ import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 
 import {AppComponent} from "./app.component";
+import {RouterModule} from "@angular/router";
 
 
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
-
-
+    BrowserModule.withServerTransition({ appId: 'TM-clint' }),
+    RouterModule.forRoot([], {initialNavigation: 'enabled'})
   ],
   providers: [],
-  bootstrap: [AppComponent],
 })
 export class AppModule {
 }
