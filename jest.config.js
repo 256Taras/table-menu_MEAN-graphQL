89700ccb177404e880d122d2c6eafe26f036e507
Taras Ivaniuk <tarasivaniuk37@gmail.com>
2021-07-25 18:58:56 +0300

    feat(root-store): add user-store [WiP] * add user-store.module.ts
    #20

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,6 @@ module.exports = {
     '<rootDir>/libs/ui-component',
     '<rootDir>/libs/shared/data-access/interfaces',
     '<rootDir>/libs/frontend/shared/data-acess/root-store',
+    '<rootDir>/libs/frontend/shared/data-access/user-store',
   ],
 };
