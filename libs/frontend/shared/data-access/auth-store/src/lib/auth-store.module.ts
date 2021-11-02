e1e7dccb4020e9e13889d5249c50cb7553575587
Taras Ivaniuk <47381043+Ezen1programmer@users.noreply.github.com>
2021-11-02 22:57:02 +0200

    feat(auth-store): add ModuleWithProviders [WiP]
    * add auth-store-options.interface.ts
    * change BaseAuthStorageService => BaseAuthStorage
    * add => auth-store.module.ts => forRoot
    * add provide: IAuthStorage => providers
     => auth-store.module.ts
    
    #17

--- libs/frontend/shared/data-access/auth-store/src/lib/auth-store.module.ts
+++ libs/frontend/shared/data-access/auth-store/src/lib/auth-store.module.ts
@@ -1,17 +1,34 @@
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+
 import * as fromAuth from './+state/auth.reducer';
 import { AuthEffects } from './+state/auth.effects';
 import { AuthFacade } from './+state/auth.facade';
+
+import { BaseAuthStorageService } from './services/base-auth-storage.service';
+import { IAuthStoreOption } from './interfaces/auth-store-option.interface';
+import { IAuthStorage } from './interfaces/auth-storage.interface';
 
 @NgModule({
   imports: [
     CommonModule,
     StoreModule.forFeature(fromAuth.AUTH_FEATURE_KEY, fromAuth.reducer),
-    EffectsModule.forFeature([AuthEffects]),
+    EffectsModule.forFeature([AuthEffects])
   ],
-  providers: [AuthFacade],
+  providers: [AuthFacade]
 })
-export class AuthStoreModule {}
+export class AuthStoreModule {
+  static forRoot(options: Partial<IAuthStoreOption> = {}): ModuleWithProviders<AuthStoreModule> {
+    return {
+      ngModule: AuthStoreModule,
+      providers: [
+        {
+          provide: IAuthStorage,
+          useClass: options.storage || BaseAuthStorageService
+        }
+      ]
+    };
+  }
+}
