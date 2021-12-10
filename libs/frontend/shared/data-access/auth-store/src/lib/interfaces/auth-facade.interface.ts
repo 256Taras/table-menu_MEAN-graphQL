6829820bf3134b20ed2c683ff20bd16dfd899067
Taras Ivaniuk <47381043+Ezen1programmer@users.noreply.github.com>
2021-12-10 15:29:58 +0200

    feat(auth-block*auth-store): change auth libs [WiP]
    * add auth-facade.interface.ts

--- libs/frontend/shared/data-access/auth-store/src/lib/interfaces/auth-facade.interface.ts
+++ libs/frontend/shared/data-access/auth-store/src/lib/interfaces/auth-facade.interface.ts
@@ -1,3 +1,57 @@
-export abstract class IAuthFacade {
+import { Observable } from 'rxjs';
+import { ApolloError } from '@apollo/client';
 
+import { ISignAuthPayload } from '@mean/shared/utils/interfaces';
+
+/**
+ * User facade interface
+ */
+export abstract class IAuthFacade {
+  /**
+   * SignIn observer
+   */
+  public signIn$!: Observable<ISignAuthPayload>;
+
+  /**
+   * SignInRun status observer
+   */
+  public signInRun$!: Observable<boolean>;
+
+  /**
+   * SignInError observer
+   */
+  public signInError$!: Observable<ApolloError>;
+
+  /**
+   * SignOut status observer
+   */
+  public signOutRun$!: Observable<boolean>;
+
+  /**
+   * SignOut error observer
+   */
+  public signOutError$!: Observable<ApolloError>;
+
+  /**
+   * Sign in
+   * @param force
+   */
+  public abstract signIn(force?: boolean): void
+
+  /**
+   * Set sign in payload
+   * @param payload
+   */
+  public abstract signInSet(payload: ISignAuthPayload): void
+
+  /**
+   * Clear sign in payload
+   */
+  public abstract signInClear(): void
+
+  /**
+   * Sign out
+   * @param force
+   */
+  public abstract signOut(force?: boolean): void
 }
